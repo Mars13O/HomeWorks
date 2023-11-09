7d9bdf600999e77c9450c4b905af4a474681a09a
Natalija93 <Gjorgjieva93@gmail.com>
2023-11-09 11:22:57 -0500

    updated find max in natalija file

--- src/natalija/week6/Array_FindMaximum.java
+++ src/natalija/week6/Array_FindMaximum.java
@@ -2,18 +2,24 @@ package natalija.week6;
 
 public class Array_FindMaximum {
 
+    /*
+    Array -- Find Maximum
+    Write a method that can find the maximum number from an int Array
+
+     */
+
     public static void main(String[] args) {
         int[] array = {6, 13, -2, -9, 0};
         System.out.println("maxNum(array) = " + maxNum(array));
     }
 
 
-    public static int maxNum(int[] array){
+    public static int maxNum(int[] array) {
 
         int max = array[0];
 
         for (int each : array) {
-            if(each > max){
+            if (each > max) {
                 max = each;
             }
         }
