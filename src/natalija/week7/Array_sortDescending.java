2a7891204f86acb1d792a8f9b406f0ed1705dab8
natalija <gjorgjieva93@gmail.com>
2023-11-16 12:20:42 -0500

    week 7 natalija

---
+++ src/natalija/week7/Array_sortDescending.java
@@ -0,0 +1,38 @@
+package natalija.week7;
+
+import java.util.Arrays;
+
+public class Array_sortDescending {
+
+    /*
+    Question3: Array - Sort Descending
+Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
+Ex: int[] arr = {10,20,7, 8, 90};
+ arr = Sort(arr); ==> {90, 20, 10, 8, 7};
+     */
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortDescending(10,20,7, 8, 90)));
+    }
+
+    public static int[] sortDescending(int... arr){
+
+        for(int i = 0; i < arr.length; i++){
+
+            for(int j = 0; j < arr.length - 1; j++){
+
+                if(arr[j] < arr[j+1]){
+
+                    int temporary = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temporary;
+
+                }
+
+            }
+
+        }
+        return arr;
+    }
+
+}
