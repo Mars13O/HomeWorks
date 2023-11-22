ac45f6a07cdc6a1e0621debc5ce20187ffc3d430
IlonaCydeo <nefertete13@gmail.com>
2023-11-22 17:17:39 -0500

    Task 1 done

--- src/week8/IlonaWeek08.java
+++ src/week8/IlonaWeek08.java
@@ -1,4 +1,32 @@
 package week8;
 
+import java.util.Arrays;
+
 public class IlonaWeek08 {
-}
+    public static void main(String[] args) {
+        //#1 --Array - N unique integers that sum up to 0
+        int N_number = 3;
+        int[] result = new int[N_number];
+
+
+        if (!(N_number > 1 || N_number < 100)) {
+            throw new IllegalArgumentException("N_number should be between 1 and 100");
+        }
+        System.out.println(Arrays.toString(result));
+
+        int sum = 0;
+
+        for (int i = 0; i < N_number - 1; i++) {
+            int randomNum = (int) (Math.random() * 10) + 1;//generate random integer
+            System.out.println(randomNum);
+            result[i] = (i % 2 == 0) ? randomNum : -randomNum;
+            sum += result[i];
+        }
+            result[N_number - 1] = -sum;
+        System.out.println(Arrays.toString(result));
+        }
+    }
+
+
+
+
