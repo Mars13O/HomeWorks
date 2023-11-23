62b0637cfd67756533ca4941e0d1c4d950221f82
IlonaCydeo <nefertete13@gmail.com>
2023-11-23 09:35:20 -0500

    Task 1 done And Task 2 is done

--- src/week8/IlonaWeek08.java
+++ src/week8/IlonaWeek08.java
@@ -12,13 +12,12 @@ public class IlonaWeek08 {
         if (!(N_number > 1 || N_number < 100)) {
             throw new IllegalArgumentException("N_number should be between 1 and 100");
         }
-        System.out.println(Arrays.toString(result));
+
 
         int sum = 0;
 
         for (int i = 0; i < N_number - 1; i++) {
             int randomNum = (int) (Math.random() * 10) + 1;//generate random integer
-            System.out.println(randomNum);
             result[i] = (i % 2 == 0) ? randomNum : -randomNum;
             sum += result[i];
         }
@@ -26,6 +25,19 @@ public class IlonaWeek08 {
 
         System.out.println(Arrays.toString(result)); //print Array
 
+        //2 Array - Move Zeros to the End
+         int [] GivenArray = {1,0,5,2,0,5,0};
+
+        int[] NumbersMoreThanZero =new int[GivenArray.length];
+        int nonZeroIndex = 0;
+        for (int i = 0; i < GivenArray.length-1; i++) {
+if((GivenArray[i] != 0)){
+    NumbersMoreThanZero[nonZeroIndex] = GivenArray[i];
+     nonZeroIndex++;
+}
+        }
+
+        System.out.println(Arrays.toString(NumbersMoreThanZero));
         }
     }
 
