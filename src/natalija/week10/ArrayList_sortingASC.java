94701e0bc667d86aeb3bc6aeefd3be27e7d5d7ed
natalija <gjorgjieva93@gmail.com>
2023-12-06 22:07:15 -0500

    week 10, tasks: ArrayList - sort ASC and sort DESC, natalija uploaded

--- src/natalija/week10/ArrayList_sortingASC.java
+++ src/natalija/week10/ArrayList_sortingASC.java
@@ -12,34 +12,6 @@ public class ArrayList_sortingASC {
     Write a method that can sort the ArrayList in Ascending order without using the sort method.
      */
 
-/*
-    public static void main(String[] args) {
-            ArrayList al = new ArrayList();
-            al.add("reena");
-            al.add("teena");
-            al.add("anu");
-            al.add("zahir");
-            al.add("sonu");
-            System.out.println("Unsorted Arraylist:");
-            System.out.println(al);
-            int count = al.size();
-            String temp;
-            Object[] name = al.toArray();
-            for (int i = 0; i < count; i++) {
-                for (int j = i + 1; j < count; j++) {
-                    if (((String) name[i]).compareTo((String) name[j]) > 0) {
-                        temp = (String) name[i];
-                        name[i] = name[j];
-                        name[j] = temp;
-                    }
-                }
-            }
-            System.out.println("Sorted Arraylist:");
-            for (int a = 0; a < name.length; a++) {
-                System.out.println(name[a]);
-            }
-        }
- */
 public static void main(String[] args) {
     ArrayList<String> arrayList = new ArrayList<>();
     arrayList.add("Natalija");
