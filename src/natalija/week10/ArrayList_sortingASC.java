a30974809881f486f5a9ee8794eab7e1df7c9045
natalija <gjorgjieva93@gmail.com>
2023-12-06 21:57:22 -0500

    week 10, tasks: ArrayList - sort ASC and sor DESC, natalija uploaded

---
+++ src/natalija/week10/ArrayList_sortingASC.java
@@ -0,0 +1,77 @@
+package natalija.week10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class ArrayList_sortingASC {
+
+    /*
+    ArrayList - sorting in ascending
+    Write a method that can sort the ArrayList in Ascending order without using the sort method.
+     */
+
+/*
+    public static void main(String[] args) {
+            ArrayList al = new ArrayList();
+            al.add("reena");
+            al.add("teena");
+            al.add("anu");
+            al.add("zahir");
+            al.add("sonu");
+            System.out.println("Unsorted Arraylist:");
+            System.out.println(al);
+            int count = al.size();
+            String temp;
+            Object[] name = al.toArray();
+            for (int i = 0; i < count; i++) {
+                for (int j = i + 1; j < count; j++) {
+                    if (((String) name[i]).compareTo((String) name[j]) > 0) {
+                        temp = (String) name[i];
+                        name[i] = name[j];
+                        name[j] = temp;
+                    }
+                }
+            }
+            System.out.println("Sorted Arraylist:");
+            for (int a = 0; a < name.length; a++) {
+                System.out.println(name[a]);
+            }
+        }
+ */
+public static void main(String[] args) {
+    ArrayList<String> arrayList = new ArrayList<>();
+    arrayList.add("Natalija");
+    arrayList.add("Marija");
+    arrayList.add("Tom");
+    arrayList.add("Petar");
+    arrayList.add("Mike");
+
+    System.out.println("Unsorted arrayList = " + arrayList);
+
+    sortArrayListASC(arrayList);
+    System.out.println("Sorted arrayList = " + arrayList);
+
+
+}
+
+    public static void sortArrayListASC(ArrayList<String> arrayList){
+
+        int count = arrayList.size();
+
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+
+                if(arrayList.get(j).compareTo(arrayList.get(j+1)) > 0){
+                    String temporary = arrayList.get(j);
+                    arrayList.set(j, arrayList.get(j + 1));
+                    arrayList.set(j + 1, temporary);
+                }
+
+            }
+        }
+
+    }
+
+}
---
+++ src/natalija/week10/ArrayList_sortingDESC.java
@@ -0,0 +1,47 @@
+package natalija.week10;
+
+import java.util.ArrayList;
+
+public class ArrayList_sortingDESC {
+
+    /*
+    ArrayList - sorting in descending order
+    Write a method that can sort the ArrayList in descending order without using the sort method.
+     */
+
+
+    public static void main(String[] args) {
+        ArrayList<String> arrList = new ArrayList<>();
+        arrList.add("Natalija");
+        arrList.add("Marija");
+        arrList.add("Tom");
+        arrList.add("Petar");
+        arrList.add("Mike");
+
+        System.out.println("Unsorted arrList = " + arrList);
+
+        sortArrayListDESC(arrList);
+        System.out.println("Sorted arrList = " + arrList);
+
+
+    }
+
+    public static void sortArrayListDESC(ArrayList<String> arrList){
+
+        int count = arrList.size();
+
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+
+                if(arrList.get(j).compareTo(arrList.get(j+1)) < 0){
+                    String temporary = arrList.get(j);
+                    arrList.set(j, arrList.get(j + 1));
+                    arrList.set(j + 1, temporary);
+                }
+
+            }
+        }
+
+    }
+
+}
